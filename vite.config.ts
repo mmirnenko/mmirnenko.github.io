76d41fd6f17896d7db6674d0d54cc55ea5550535
mmirnenko <markmirnenko21@gmail.com>
2026-07-07 13:43:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+     base: '/mmirnenko.github.io/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
